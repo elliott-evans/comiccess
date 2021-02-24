1ae8b93d65788330a3054f9d287da3f6eba442f4
elliott-evans <48378115+elliott-evans@users.noreply.github.com>
2021-02-25 09:41:35 +1300

    Add files via upload

---
+++ comiccess.py
@@ -0,0 +1,23 @@
+#***     Import Libraries & Functions     ***#
+from tkinter import *
+import random
+import csv
+import winsound
+from functools import partial
+
+#***     Load Data From Files     ***#
+
+
+#***     Define Classes     ***#
+
+
+#***     Define Functions     ***#
+
+
+#***     TKinter Window     ***#
+
+
+
+#***     Run Updates & TKinter Window     ***#
+print("Test")
+#root.mainloop()
