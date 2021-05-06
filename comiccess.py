765610ce13cb18505212528662997cadc9a99451
elliott-evans <48378115+elliott-evans@users.noreply.github.com>
2021-05-07 10:33:57 +1200

    Add files via upload

--- comiccess.py
+++ comiccess.py
@@ -1,9 +1,9 @@
 #***     IMPORT LIBRARIES     ***#
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 import random
 import csv
-import winsound
 from functools import partial
 
 #***     INITIALISE TKINTER WINDOW     ***#
@@ -14,31 +14,61 @@ root.resizable(False, False)
 
 #***     DEFINE VARIABLES     ***#
 search_entry = StringVar()
+selectitem_existing_number_var = StringVar()
+selectitem_create_volume_num_var = StringVar()
+selectitem_create_volume_name_var = StringVar()
+selectitem_create_init_stock_var = StringVar()
 tab_startup_status = True
 stock_items_class_list = []
+series_list = {}
+series_select_list = [""]
+vol_select_list = []
 
+selectitem_series_existing_var = StringVar()
 #***     DEFINE CLASSES     ***#
 class Comic:
     def __init__(self, series_name, vol_number, issue_name, in_stock, historical_sales, title_data, search_title):
         self.series_name = series_name
         self.vol_number = vol_number
         self.issue_name = issue_name
+        self.vol_issue = 'Vol. ' + self.vol_number + ': ' + self.issue_name
         self.full_title = str(series_name + ' Vol. ' + str(vol_number) + ': ' + issue_name)
-        self.search_title = self.full_title.strip()
-        self.in_stock = in_stock
+        self.in_stock = int(in_stock)
         self.historical_sales = historical_sales
         self.title_data = title_data
         self.search_title = search_title
+        if self.series_name not in series_list:
+            series_list[self.series_name] = {self.vol_issue: self}
+        elif self.series_name in series_list and self.vol_issue not in series_list[self.series_name]:
+            series_list[self.series_name].append({self.vol_issue: self})
+        else:
+            result = messagebox.askquestion('Comic Duplicate Error','Instance of the {} comic already detected! Would you like to delete the duplicate item?'.format(self.full_title))
+            if result=='yes':
+                del self
+            else:
+                pass
 
-        self.index_frame = ttk.Frame(index_results_frame, relief=SUNKEN)
-        self.index_title = ttk.Label(self.index_frame, text=self.full_title, anchor=CENTER, font='Bahnschrift 10 bold').grid(column=0, row=0, columnspan=2, sticky="ew")
-        self.index_stock_label = ttk.Label(self.index_frame, text="In Stock", anchor=CENTER).grid(column=0, row=1, sticky="ew")
-        self.index_hist_label = ttk.Label(self.index_frame, text="Historic Sales", anchor=CENTER).grid(column=1, row=1, sticky="ew")
-        self.index_stock_item = ttk.Label(self.index_frame, text=self.in_stock, anchor=CENTER).grid(column=0, row=2, sticky="ew")
-        self.index_hist_item = ttk.Label(self.index_frame, text=self.historical_sales, anchor=CENTER).grid(column=1, row=2, sticky="ew")
+    def create_item_widget(self, location):
+        self.item_frame = ttk.Frame(location, relief=SUNKEN)
+        self.mod_title = ttk.Label(self.item_frame, text=self.full_title, anchor=CENTER, font='Bahnschrift 10 bold').grid(column=0, row=0, columnspan=2, sticky="ew")
+        self.mod_stock_label = ttk.Label(self.item_frame, text="In Stock", anchor=CENTER).grid(column=0, row=1, sticky="ew")
+        self.mod_hist_label = ttk.Label(self.item_frame, text="Historic Sales", anchor=CENTER).grid(column=1, row=1, sticky="ew")
+        self.mod_stock_item = ttk.Label(self.item_frame, text=self.in_stock, anchor=CENTER).grid(column=0, row=2, sticky="ew")
+        self.mod_hist_item = ttk.Label(self.item_frame, text=self.historical_sales, anchor=CENTER).grid(column=1, row=2, sticky="ew")
+        self.item_frame.pack()
+        
+    def clear_all():
+        self.item_frame.forget_pack()
+        del self
     
 #***     LOAD DATA FROM FILES     ***#
 def load_stock_data():
+    for item in stock_items_class_list:
+        item.clear_all()
+    stock_items_class_list = []
+    series_list = {}
+    series_select_list = [""]
+    vol_select_list = []
     with open('stock.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
@@ -55,10 +85,19 @@ def load_stock_data():
         print(f'Processed {line_count-1} item/s.')
         topline = ttk.Label(index_results_frame, text="––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––").pack()
         for i in range(len(stock_items_class_list)):
-            stock_items_class_list[i][1].index_frame.pack()
+            stock_items_class_list[i][1].create_item_widget(index_results_frame)
             ttk.Separator(index_results_frame, orient=HORIZONTAL).pack(fill="both", expand=True)
+        update_lists()
 
 #***     DEFINE FUNCTIONS     ***#
+def update_lists():
+    global series_list, series_select_list
+    for item in series_list.keys():
+        series_select_list.append(item)
+    selectitem_existing_series.config(values=series_select_list)
+    selectitem_create_series.config(values=series_select_list)
+    print("e{}".format(series_select_list))
+
 def do_tab_switch_sales(event):
     global tab_startup_status
     if tab_startup_status == False:
@@ -81,6 +120,156 @@ def do_tab_switch_modify(event):
         print('whoops')
     print(event)
 
+#* Comics Selection Functions *#
+def existing_series_selected(event):
+    global vol_select_list
+    if selectitem_existing_series.get() == "":
+        vol_select_list = [""]
+        selectitem_existing_vol.config(values=vol_select_list)
+        selectitem_existing_vol.set("")
+        selectitem_existing_number.delete(0, END)
+        selectitem_existing_operator.set("")
+        selectitem_existing_vol.config(state='disabled')
+        selectitem_existing_operator.config(state='disabled')
+        selectitem_existing_number.config(state='disabled')
+        selectitem_apply_changes_button.config(state='disabled')
+        selectitem_create_series.config(state='normal')
+    else:
+        vol_select_list = [""]
+        selectitem_existing_vol.config(state='readonly')
+        for key, val_list in series_list.items():
+            if selectitem_existing_series.get() == key:
+                for item in val_list.keys():
+                    vol_select_list.append(item)
+        selectitem_existing_vol.config(values=vol_select_list)
+        selectitem_create_series.set("")
+        selectitem_create_series.config(state='disabled')
+        
+    print(event)
+
+def existing_vol_selected(event):
+    if selectitem_existing_vol.get() == "":
+        selectitem_existing_operator.config(state='disabled')
+        selectitem_existing_operator.set("")
+        selectitem_existing_number.delete(0, END)
+        selectitem_existing_number.config(state='disabled')
+        selectitem_apply_changes_button.config(state='disabled')
+    else:
+        selectitem_existing_operator.config(state='readonly')
+
+def existing_operator_selected(event):
+    if selectitem_existing_operator.get() == "":
+        selectitem_existing_number.delete(0, END)
+        selectitem_existing_number.config(state='disabled')
+        selectitem_apply_changes_button.config(state='disabled')
+    else:
+        selectitem_existing_number.config(state='normal')
+    
+def existing_number_validate(char):
+    if char.isdigit():
+        return True
+    else:
+        return False
+
+def selectitem_existing_num_command(event):
+    if selectitem_existing_number.get() == "":
+        selectitem_apply_changes_button.config(state='disabled')
+    else:
+        selectitem_apply_changes_button.config(state='normal')
+        
+
+def created_series_selected(event):
+    if selectitem_create_series.get() == "":
+        selectitem_create_series.config(state='normal')
+        selectitem_create_vol_entry.delete(0, END)
+        selectitem_create_vol_entry.config(state='disabled')
+        selectitem_create_vol_name.delete(0, END)
+        selectitem_create_vol_name.config(state='disabled')
+        selectitem_create_init_stock.delete(0, END)
+        selectitem_create_init_stock.config(state='disabled')
+        selectitem_apply_changes_button.config(state='disabled')
+        selectitem_existing_series.config(state='readonly')
+        
+    elif selectitem_create_series.get() in series_select_list:
+        selectitem_create_series.config(state='readonly')
+        selectitem_create_vol_entry.config(state='normal')
+        selectitem_existing_series.set("")
+        selectitem_existing_series.config(state='disabled')
+    else:
+        selectitem_create_series.config(state='normal')
+        selectitem_create_vol_entry.config(state='normal')
+        selectitem_existing_series.set("")
+        selectitem_existing_series.config(state='disabled')
+
+def created_vol_num_selected(event):
+    if selectitem_create_vol_entry.get() == "":
+        selectitem_create_vol_name.delete(0, END)
+        selectitem_create_vol_name.config(state='disabled')
+        selectitem_create_init_stock.delete(0, END)
+        selectitem_create_init_stock.config(state='disabled')
+        selectitem_apply_changes_button.config(state='disabled')
+    else:
+        selectitem_create_vol_name.config(state='normal')
+        
+def created_vol_name_selected(event):
+    if selectitem_create_vol_name.get() == "":
+        selectitem_create_init_stock.delete(0, END)
+        selectitem_create_init_stock.config(state='disabled')
+        selectitem_apply_changes_button.config(state='disabled')
+    else:
+        selectitem_create_init_stock.config(state='normal')
+    
+def created_comic_init_stock_selected(event):
+    if selectitem_create_init_stock.get() == "":
+        selectitem_apply_changes_button.config(state='disabled')
+    else:
+        selectitem_apply_changes_button.config(state='normal')
+
+def selectitem_apply_changes():
+    if selectitem_existing_series.get() != "":
+        print(series_list[selectitem_existing_series.get()][selectitem_existing_vol.get()].in_stock)
+        comic_class_instance = series_list[selectitem_existing_series.get()][selectitem_existing_vol.get()]
+        stock_action = selectitem_existing_operator.get()
+        stock_number = int(selectitem_existing_number.get())
+        do_clear = False
+        if stock_action == "SET":
+            comic_class_instance.in_stock = stock_number
+            do_clear = True
+        elif stock_action == "+":
+            comic_class_instance.in_stock += stock_number
+            do_clear = True
+        elif stock_action == "-":
+            if stock_number <= comic_class_instance.in_stock:
+                comic_class_instance.in_stock -= stock_number
+                do_clear = True
+            else:
+                messagebox.showerror('Stock Error!', 'You tried to remove more comics than were in stock. Try again.')
+                do_clear = False
+                selectitem_existing_number.delete(0, END)
+                selectitem_existing_num_command("")
+        print(series_list[selectitem_existing_series.get()][selectitem_existing_vol.get()].in_stock)
+        if do_clear == True:
+            selectitem_existing_series.set("")
+            existing_series_selected("")
+            update_save_data()
+    elif selectitem_create_series.get() != "":
+        series_name = selectitem_create_series.get()
+        vol_number = selectitem_create_vol_entry.get()
+        issue_name = selectitem_create_vol_name.get()
+        in_stock = selectitem_create_init_stock.get()
+        title_data = str(series_name + ' Vol. ' + vol_number + ': ' + issue_name + "\n - " + in_stock + " in stock, and " + "0" + " have been sold.")
+        full_title_var = str(series_name + vol_number + issue_name).strip(",. ").lower().replace(",", "").replace(".", "").replace(" ","")
+        globals()[full_title_var] = Comic(series_name, vol_number, issue_name, in_stock, 0, title_data, full_title_var)
+        update_save_data()
+        selectitem_create_series.set("")
+        created_series_selected("") 
+    else: 
+        pass
+
+def update_save_data():
+    print("Data (not) saved!")
+    #load_stock_data()
+
 #***     TKINTER WINDOW     ***#
 s = ttk.Style()
 s.configure('new.TFrame', background='#7AC5CD')
@@ -92,10 +281,11 @@ root.grid_columnconfigure(0, weight=1)
 
 parent_separator = ttk.Separator(root, orient=VERTICAL)
 parent_separator.grid(column=1, row=0, sticky='ns', padx=5, pady=5)
+root.grid_columnconfigure(1, weight=0)
 
 modify_parent = ttk.Frame(root)
 modify_parent.grid(column=2, row=0, sticky=N)
-root.grid_columnconfigure(2, weight=3)
+root.grid_columnconfigure(2, weight=7)
 root.grid_rowconfigure(0, weight=1)
 
 #* Index Frames *#
@@ -126,8 +316,6 @@ index_results_canvas.configure(yscrollcommand=index_results_scrollbar.set)
 
 index_results_canvas.pack(side="left", fill="both", expand=True)
 index_results_scrollbar.pack(side="right", fill="y")
-
-
 
 #* Modify Frames *#
 #* Modify Tab Button Frames *#
@@ -160,9 +348,87 @@ modify_sales_tab = ttk.Frame(modify_frames_parent)
 modify_sales_tab.pack(side="left", fill="both", expand=True)
 
 modify_manage_stock_tab = ttk.Frame(modify_frames_parent)
-modify_manage_stock_tab.pack(side="left", fill="both", expand=True)
-modify_manage_stock_tab.forget()
+#* Manage Stock Parent Frames *#
+manage_stock_selectitem_parent = ttk.LabelFrame(modify_manage_stock_tab, text="Select Comic")
+manage_stock_selectitem_parent.grid(row=0, column=0)
+
+# Manage Stock SelectItem Parent Frames #
+selectitem_existing_parent = ttk.LabelFrame(manage_stock_selectitem_parent, text="Existing Comic")
+selectitem_existing_parent.grid(row=0, column=0)
+
+selectitem_or_divider = ttk.Label(manage_stock_selectitem_parent, text=" or ")
+selectitem_or_divider.grid(row=0, column=1)
+
+selectitem_new_parent = ttk.LabelFrame(manage_stock_selectitem_parent, text="Create Comic")
+selectitem_new_parent.grid(row=0, column=2)
+
+# SelectItem Existing #
+selectitem_existing_series_label = ttk.Label(selectitem_existing_parent, text="Series:  ")
+selectitem_existing_series_label.grid(row=0, column=0)
+
+selectitem_existing_series = ttk.Combobox(selectitem_existing_parent, state='readonly', values=series_select_list)
+selectitem_existing_series.grid(row=0, column=1, pady=5)
+selectitem_existing_series.bind("<<ComboboxSelected>>",existing_series_selected)
+
+selectitem_existing_vol_label = ttk.Label(selectitem_existing_parent, text="Volume:  ")
+selectitem_existing_vol_label.grid(row=1, column=0)
+
+selectitem_existing_vol = ttk.Combobox(selectitem_existing_parent, state='disabled')
+selectitem_existing_vol.grid(row=1, column=1, pady=5)
+selectitem_existing_vol.bind("<<ComboboxSelected>>",existing_vol_selected)
+
+selectitem_existing_operator = ttk.Combobox(selectitem_existing_parent, state='disabled', values=("SET","+","-"), width=4)
+selectitem_existing_operator.grid(row=2, column=0, pady=5)
+selectitem_existing_operator.bind("<<ComboboxSelected>>",existing_operator_selected)
+
+selectitem_existing_number_validation = selectitem_existing_parent.register(existing_number_validate)
+selectitem_existing_number = ttk.Entry(selectitem_existing_parent, textvariable=selectitem_existing_number_var, state='disabled', width=23, validate='all', validatecommand=(selectitem_existing_number_validation, '%S'))
+selectitem_existing_number.grid(row=2, column=1, pady=5)
+selectitem_existing_number.bind("<KeyRelease>",selectitem_existing_num_command)
+                
+# SelectItem CreateNew #
+selectitem_create_series_label = ttk.Label(selectitem_new_parent, text="Series:  ")
+selectitem_create_series_label.grid(row=0, column=0)
+
+selectitem_create_series = ttk.Combobox(selectitem_new_parent, values=series_select_list)
+selectitem_create_series.grid(row=0, column=1, pady=5)
+selectitem_create_series.bind("<<ComboboxSelected>>",created_series_selected)
+selectitem_create_series.bind("<KeyRelease>",created_series_selected)
+
+selectitem_create_vol_frame = ttk.Frame(selectitem_new_parent)
+selectitem_create_vol_frame.grid(row=1, column=0, columnspan=2)
+
+selectitem_create_vol_label = ttk.Label(selectitem_create_vol_frame, text="  Vol.")
+selectitem_create_vol_label.grid(row=0, column=0)
+
+selectitem_create_vol_entry_validation = selectitem_create_vol_frame.register(existing_number_validate)
+selectitem_create_vol_entry = ttk.Entry(selectitem_create_vol_frame, textvariable=selectitem_create_volume_num_var, state='disabled', width=3, validate='all', validatecommand=(selectitem_create_vol_entry_validation, '%S'))
+selectitem_create_vol_entry.grid(row=0, column=1)
+selectitem_create_vol_entry.bind("<KeyRelease>",created_vol_num_selected)
+
+selectitem_create_vol_name_label = ttk.Label(selectitem_create_vol_frame, text=" : ")
+selectitem_create_vol_name_label.grid(row=0, column=2)
+
+selectitem_create_vol_name = ttk.Entry(selectitem_create_vol_frame, textvariable=selectitem_create_volume_name_var, state='disabled', width=23)
+selectitem_create_vol_name.grid(row=0, column=3)
+selectitem_create_vol_name.bind("<KeyRelease>",created_vol_name_selected)
+
+selectitem_create_init_stock_label = ttk.Label(selectitem_new_parent, text="Initial Stock:")
+selectitem_create_init_stock_label.grid(row=2, column=0)
+
+selectitem_create_init_stock_validation = selectitem_new_parent.register(existing_number_validate)
+selectitem_create_init_stock = ttk.Entry(selectitem_new_parent, width=23, textvariable=selectitem_create_init_stock_var, state='disabled', validate='all', validatecommand=(selectitem_create_init_stock_validation, '%S'))
+selectitem_create_init_stock.grid(row=2, column=1, pady=5)
+selectitem_create_init_stock.bind("<KeyRelease>",created_comic_init_stock_selected)
+
+# SelectItem Apply Changes #
+selectitem_apply_changes_button = ttk.Button(manage_stock_selectitem_parent, text="Apply Changes", command=selectitem_apply_changes, state='disabled', width=70)
+selectitem_apply_changes_button.grid(row=2, column=0, columnspan=3, pady=5)
 
 #***     RUN UPDATES & TKINTER WINDOW     ***#
+do_tab_switch_modify("")
+do_tab_switch_sales("")
 load_stock_data()
+print("\n E")
+print(series_list)
 root.mainloop()
